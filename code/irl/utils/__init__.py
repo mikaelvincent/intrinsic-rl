065797900faf71b4675236da21212db7c76d8993
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2025-11-07 14:11:42 +0800

    Export torchops helpers from utils package
    * Re-export `as_tensor`, `ensure_2d`, `one_hot` in `__all__`.
    * Enables convenient imports (`from irl.utils import as_tensor, ...`).
    * No breaking changes to existing imports.

--- code/irl/utils/__init__.py
+++ code/irl/utils/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from .loggers import CSVLogger, TBLogger, MetricLogger
 from .checkpoint import CheckpointManager, load_checkpoint
+from .torchops import as_tensor, ensure_2d, one_hot  # shared tensor helpers
 
 __all__ = [
     # Loggers
@@ -13,4 +14,8 @@ __all__ = [
     # Checkpointing
     "CheckpointManager",
     "load_checkpoint",
+    # Tensor helpers
+    "as_tensor",
+    "ensure_2d",
+    "one_hot",
 ]
