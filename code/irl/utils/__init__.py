0cdfcfb7ab3e3c8b580bec0e2b6230267ae8e78e
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2025-11-08 16:54:16 +0000

    Export atomic write helpers
    * Re-export `atomic_replace`, `atomic_write_text`, and `atomic_write_bytes`.
    * No new dependencies.

--- code/irl/utils/__init__.py
+++ code/irl/utils/__init__.py
@@ -3,7 +3,13 @@
 from __future__ import annotations
 
 from .loggers import CSVLogger, TBLogger, MetricLogger
-from .checkpoint import CheckpointManager, load_checkpoint
+from .checkpoint import (
+    CheckpointManager,
+    load_checkpoint,
+    atomic_replace,
+    atomic_write_text,
+    atomic_write_bytes,
+)
 from .torchops import as_tensor, ensure_2d, one_hot  # shared tensor helpers
 
 __all__ = [
@@ -14,6 +20,9 @@ __all__ = [
     # Checkpointing
     "CheckpointManager",
     "load_checkpoint",
+    "atomic_replace",
+    "atomic_write_text",
+    "atomic_write_bytes",
     # Tensor helpers
     "as_tensor",
     "ensure_2d",
