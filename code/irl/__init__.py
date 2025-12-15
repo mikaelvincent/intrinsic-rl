b52a3112f7f56fed35213214aa38f951649a00f4
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2025-12-16 02:14:47 +0800

    Clean package init exports
    * Remove unnecessary vertical padding
    * Keep the NNPACK workaround and public exports unchanged

--- code/irl/__init__.py
+++ code/irl/__init__.py
@@ -1,17 +1,10 @@
 import os
 
-# Avoid NNPACK crashes/hangs on some CPUs.
+# Work around NNPACK hangs/crashes on some CPUs.
 os.environ["ATEN_NNPACK_ENABLED"] = "0"
 
 from ._version import __version__
 from .utils.checkpoint import CheckpointManager, load_checkpoint
 from .utils.loggers import CSVLogger, MetricLogger, TBLogger
 
-__all__ = (
-    "__version__",
-    "CSVLogger",
-    "TBLogger",
-    "MetricLogger",
-    "CheckpointManager",
-    "load_checkpoint",
-)
+__all__ = ("__version__", "CSVLogger", "TBLogger", "MetricLogger", "CheckpointManager", "load_checkpoint")
