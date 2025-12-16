5598ea76bab0d6279a350d07d16a5d982db9c767
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2025-12-16 09:00:12 +0800

    Trim package init
    - Keep the NNPACK disable workaround
    - Reduce noise and format exports cleanly

--- code/irl/__init__.py
+++ code/irl/__init__.py
@@ -7,4 +7,11 @@ from ._version import __version__
 from .utils.checkpoint import CheckpointManager, load_checkpoint
 from .utils.loggers import CSVLogger, MetricLogger, TBLogger
 
-__all__ = ("__version__", "CSVLogger", "TBLogger", "MetricLogger", "CheckpointManager", "load_checkpoint")
+__all__ = (
+    "__version__",
+    "CSVLogger",
+    "TBLogger",
+    "MetricLogger",
+    "CheckpointManager",
+    "load_checkpoint",
+)
