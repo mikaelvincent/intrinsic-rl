ddf94090317360bf56e0b940426e13eb668434be
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2025-12-16 17:04:31 +0800

    Remove TBLogger from package exports
    * Drop TBLogger import/export to match CSV-only logging
    * Keep public exports for CSV logging and checkpoints

--- code/irl/__init__.py
+++ code/irl/__init__.py
@@ -4,12 +4,11 @@ os.environ["ATEN_NNPACK_ENABLED"] = "0"
 
 from ._version import __version__
 from .utils.checkpoint import CheckpointManager, load_checkpoint
-from .utils.loggers import CSVLogger, MetricLogger, TBLogger
+from .utils.loggers import CSVLogger, MetricLogger
 
 __all__ = (
     "__version__",
     "CSVLogger",
-    "TBLogger",
     "MetricLogger",
     "CheckpointManager",
     "load_checkpoint",
